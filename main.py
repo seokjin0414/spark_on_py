cfe0a4bd7d7c91d887feec91edd933be04695756
shin <sars21@hanmail.net>
2025-06-05 00:21:17 +0900

    test insert pyspark - dataframe

--- main.py
+++ main.py
@@ -1,47 +1,25 @@
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 from datetime import datetime
+import os
 
-# Spark Session
+load_dotenv()
+spark_connect = os.getenv("SPARK_CONNECT")
+spark = SparkSession.builder.remote(spark_connect).getOrCreate()
 
-spark = SparkSession.builder \
-    .appName("IcebergInsert") \
-    .getOrCreate()
-
-
-data_set = [
-    {
-        "measurement_point_id": "31c1eb85-836d-4140-a3a3-5eb5e8035d50",
-        "value": 12.34,
-        "recorded_at": "2024-06-04T12:00:00Z"
-    },
-    {
-        "measurement_point_id": "e788870b-7783-4bc2-81e1-56d763769c12",
-        "value": 23.45,
-        "recorded_at": "2024-06-04T13:00:00Z"
-    }
+data = [
+    ("31c1eb85-836d-4140-a3a3-5eb5e8035d50", 12.34, datetime.strptime("2024-06-04T12:00:00Z", "%Y-%m-%dT%H:%M:%SZ")),
+    ("e788870b-7783-4bc2-81e1-56d763769c12", 23.45, datetime.strptime("2024-06-04T13:00:00Z", "%Y-%m-%dT%H:%M:%SZ")),
 ]
-
 schema = StructType([
     StructField("measurement_point_id", StringType(), False),
     StructField("value", DoubleType(), True),
     StructField("recorded_at", TimestampType(), False),
 ])
 
-# 3. String → datetime 변환
-def parse_row(row):
-    return (
-        row["measurement_point_id"],
-        float(row["value"]) if row["value"] is not None else None,
-        datetime.strptime(row["recorded_at"], "%Y-%m-%dT%H:%M:%SZ"),
-    )
+df = spark.createDataFrame(data, schema=schema)
+df.write.format("iceberg").mode("append").save("v1.test")
 
-converted_data = [parse_row(r) for r in data_set]
-
-sdf = spark.createDataFrame(converted_data, schema=schema)
-
-sdf.write.format("iceberg") \
-    .mode("append") \
-    .save("s3catalog.db.table")
-
+print("#####end")
 spark.stop()
